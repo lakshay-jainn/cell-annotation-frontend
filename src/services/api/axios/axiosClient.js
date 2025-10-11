532fcef06513144ec965010c5e2712ffb69ea165
lakshay-jainn <lakshay6690@gmail.com>
2025-10-11 15:27:11 +0530

    changed api link

--- src/services/api/axios/axiosClient.js
+++ src/services/api/axios/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./authBridge";
 
 const axiosClient = axios.create({
-  baseURL: "http://54.197.100.41:8000/api/v1",
+  baseURL: "https://www.psg-ebus.work.gd/api/v1",
   timeout: 120000, // 120 second timeout (increased for image processing APIs)
 });
 
